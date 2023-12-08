363b391cf742c9521fa0cee85a10a837d24f21eb
Carlos Casaleiro <cmcasaleiro@gmail.com>
2023-12-08 11:39:17 +0000

    deploy

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/covid-tracker/",
   plugins: [react()],
   server: {
     host: true
